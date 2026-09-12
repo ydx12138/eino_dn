e9abba993cc72cc0eac4e78801ad3490e4e05251
杨栋旭 <2675723732@qq.com>
2026-09-12 20:57:52 +0800

    1.template,文本向量

--- go.mod
+++ go.mod
@@ -14,7 +14,8 @@ require (
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
-	github.com/cloudwego/eino v0.7.37 // indirect
+	github.com/cloudwego/eino v0.9.19 // indirect
+	github.com/cloudwego/eino-ext/components/embedding/ark v0.1.2 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/eino-contrib/jsonschema v1.0.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -38,8 +39,9 @@ require (
 	github.com/yargevad/filepathx v1.0.0 // indirect
 	golang.org/x/arch v0.11.0 // indirect
 	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"os"
-
-	"github.com/cloudwego/eino-ext/components/model/ark"
-	"github.com/cloudwego/eino/schema"
-	"github.com/joho/godotenv"
-)
-
 //完整生成
 /*func main() {
 	//加载配置文件
@@ -72,7 +62,7 @@ import (
 }*/
 
 // ChatTemplate
-func main() {
+/*func main() {
 	//加载配置文件
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -84,14 +74,61 @@ func main() {
 		APIKey: os.Getenv("ARK_API_KEY"),
 		Model:  os.Getenv("MODEL"),
 	})
-	//调用模型生成响应
-	input := []*schema.Message{
-		schema.SystemMessage("你是一个可爱的高中美少女"),
-		schema.UserMessage("你好"),
+
+	//参数设置
+	template := prompt.FromMessages(schema.FString,
+		schema.SystemMessage("你是一个{role}"),
+		schema.UserMessage("请帮帮我，史瓦罗先生，{task}"),
+	)
+
+	param := map[string]interface{}{
+		"role": "机器人史瓦罗先生",
+		"task": "帮我生成一首五言绝句",
 	}
-	response, err := model.Generate(ctx, input)
+	message, err := template.Format(ctx, param)
+	if err != nil {
+		return
+	}
+	//调用模型生成响应
+	response, err := model.Generate(ctx, message)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(response.String())
+	fmt.Println(response.Content)
+}*/
+
+// 文本转为向量
+/*func main() {
+	EmedText()
 }
+func EmedText() {
+	//加载配置文件
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	//创建model
+	//使用的模型是多模的，因此这里必须为MultiModal
+	apitype := emark.APITypeMultiModal
+	model, err := emark.NewEmbedder(ctx, &emark.EmbeddingConfig{
+		APIKey:  os.Getenv("ARK_API_KEY"),
+		Model:   os.Getenv("EMBEDDER"),
+		APIType: &apitype,
+	})
+	if err != nil {
+		panic(err)
+	}
+	text := []string{
+		"这",
+		"这",
+	}
+	//向量化
+	embedStrings, err := model.EmbedStrings(ctx, text)
+	if err != nil {
+		panic(err)
+	}
+	for i, embedString := range embedStrings {
+		fmt.Println("文本", i, ":", "维度:", len(embedString))
+	}
+}*/
